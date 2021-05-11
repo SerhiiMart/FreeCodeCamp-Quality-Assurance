db25ca297d2b20e9b9804e81a29a846f28018d3b
Serhii <malgothsm@gmail.com>
2021-05-11 18:44:44 +0300

    Done from 1 to 3 of second file of tests

--- boilerplate-mochachai/tests/2_functional-tests.js
+++ boilerplate-mochachai/tests/2_functional-tests.js
@@ -14,8 +14,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
           done();
         });
     });
@@ -25,8 +25,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello?name=xy_z")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello xy_z");
           done();
         });
     });
@@ -35,10 +35,20 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .put("/travellers")
-
+        .send({ surname: 'Colombo' })
         .end(function (err, res) {
-          assert.fail();
-
+              assert.equal(res.status, 200, 'response status should be 200');
+        assert.equal(res.type, 'application/json', 'Response should be json');
+        assert.equal(
+          res.body.name,
+          'Cristoforo',
+          'res.body.name should be "Christoforo"'
+        );
+        assert.equal(
+          res.body.surname,
+          'Colombo',
+          'res.body.surname should be "Colombo"'
+        );
           done();
         });
     });
