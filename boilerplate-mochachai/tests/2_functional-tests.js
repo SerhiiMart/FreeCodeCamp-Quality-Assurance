3c93dff64ec086acb740b5f8f158496dfdf2e3a9
Serhii <malgothsm@gmail.com>
2021-05-12 14:48:48 +0300

    Done the fifth

--- boilerplate-mochachai/tests/2_functional-tests.js
+++ boilerplate-mochachai/tests/2_functional-tests.js
@@ -33,53 +33,53 @@ suite("Functional Tests", function () {
     // #3
     test('send {surname: "Colombo"}', function (done) {
       chai
-      .request(server)
-      .put('/travellers')
-      .send({ surname: 'da Verrazzano' })
-        .end( (err, res) =>  {
-        assert.equal(res.status, 200, 'response status should be 200');
-        assert.equal(res.type, 'application/json', 'Response should be json');
-        assert.equal(
-          res.body.name,
-          'Cristoforo',
-          'res.body.name should be "Christoforo"'
-        );
-        assert.equal(
-          res.body.surname,
-          'Colombo',
-          'res.body.surname should be "Colombo"'
-        );
+        .request(server)
+        .put("/travellers")
+        .send({surname:"Colombo"})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json")
+          assert.equal(res.body.name,"Cristoforo")
+          assert.equal(res.body.surname,"Colombo")
+
           done();
         });
     });
     // #4
     test('send {surname: "da Verrazzano"}', function (done) {
-      chai
-        .request(server)
-        .put("/travellers")
-        .send({ surname: 'da Verrazzano' })
-        .end( (err, res) => {
-            assert.equal(res.status, 200, 'response status should be 200');
-            assert.equal(res.type, 'application/json', "Response should be json");
-            assert.equal(res.body.name, 'Giovanni', 'res.body.name should be "Giovanni" ');
-            assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be "da Verrazzano"'  );
+      chai.request(server)
+      .put("/travellers")
+      .send({surname:"da Verrazzano"})
+      .end((err,res)=>{
+        assert.equal(res.status,200)
+        assert.equal(res.type,"application/json")
+        assert.equal(res.body.name,"Giovanni")
+        assert.equal(res.body.surname,"da Verrazzano")
+      })
 
       done();
+    });
   });
-});
-});
 });
 
 const Browser = require("zombie");
+Browser.site = "https://faceted-fragrant-text.glitch.me"
 
 suite("Functional Tests with Zombie.js", function () {
-
+const browser = new Browser()
+  suiteSetup(function(done) {
+    return browser.visit("/", done)
+  })
+  
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-      browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
-
+      browser.fill("surname", "Colombo")
+         .pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text("span#name", "Cristoforo")
+        browser.assert.text("span#surname", "Colombo")
+        browser.assert.elements('span#dates', 1);
         done();
       });
     });
