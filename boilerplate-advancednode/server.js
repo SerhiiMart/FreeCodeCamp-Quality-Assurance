bbf1a4afed6b8337b7f57c73c794f03b81bcf8bd
Serhii <malgothsm@gmail.com>
2021-05-12 17:13:27 +0300

    Done the first of Advanced Node and Express

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -11,11 +11,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
+
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
