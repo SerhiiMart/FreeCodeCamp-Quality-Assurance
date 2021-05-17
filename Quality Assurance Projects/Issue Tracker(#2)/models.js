ed1307fe374347009e30b060d749857f24408c1a
Serhii <malgothsm@gmail.com>
2021-05-17 18:53:19 +0300

    added Schemas and Models

---
+++ Quality Assurance Projects/Issue Tracker(#2)/models.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema({
+  issue_title:  { type: String, require: true },
+  issue_text: { type: String, require: true },
+  created_on: { type: Date, require: true },
+  updated_on: { type: Date },
+  created_by: { type: String, require: true },
+  assigned_to: { type: String },
+  open: { type: Boolean },
+  status_text: { type: String }
+});
+
+const Issue = mongoose.model('Issue', issueSchema);
+
+const projectSchema = new Schema({
+  name:  { type: String, require: true },
+  issues: issueSchema,
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+exports.Issue = Issue;
+exports.Project = Project;
