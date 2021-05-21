8ee6d45bdb09d3bb6fadaf8428021b0d9799fc25
Serhii <malgothsm@gmail.com>
2021-05-21 13:57:28 +0300

    finished API

--- Quality Assurance Projects/Sudoku Solver(#4)/routes/api.js
+++ Quality Assurance Projects/Sudoku Solver(#4)/routes/api.js
@@ -4,15 +4,72 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
   
-  let solver = new SudokuSolver();
+ let solver = new SudokuSolver();
 
-  app.route('/api/check')
+ app.route('/api/check')
     .post((req, res) => {
+      const {puzzle, coordinate, value} = req.body; 
+      let legitPuzzle = solver.validate(puzzle); 
+      if (!puzzle || !coordinate || !value){
+        res.json({error:'Required field(s) missing'})
+      }
+      else if (legitPuzzle === "Invalid characters in puzzle"){
+        res.json({error:"Invalid characters in puzzle"})
+      } 
+      else if (legitPuzzle === "Expected puzzle to be 81 characters long"){
+        res.json({error:"Expected puzzle to be 81 characters long"}); 
+      }
+      else if (/^([A-I][1-9])$/.test(coordinate) === false){
+        res.json({error: 'Invalid coordinate'})
+      } 
+      else if (/^[1-9]$/.test(value) === false){
+        res.json({ error: 'Invalid value' })
+      }
+      else{
+        let row = coordinate.split("")[0];  
+        let column = coordinate.split("")[1];
+        let board = solver.transformToBoard(legitPuzzle);
+        let conflict = []; 
+        if (solver.checkRowPlacement(board, row, column, value)===false){
+          conflict.push("row"); 
+        }
+        if (solver.checkColPlacement(board, row, column, value)===false){
+          conflict.push("column"); 
+        }
+        if (solver.checkRegionPlacement(board, row, column, value)===false){
+          conflict.push("region"); 
+        }
 
+        if (conflict.length === 0){
+          res.json({valid:true})
+        }
+        else{
+          res.json({valid:false, conflict})
+        }
+      }
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+      let puzzle = req.body.puzzle;
+      let legitPuzzle = solver.validate(puzzle);
+      if (legitPuzzle === "Required field missing"){
+        res.json({error:'Required field missing'})
+      }
+      else if (legitPuzzle === "Invalid characters in puzzle"){
+        res.json({error:"Invalid characters in puzzle"})
+      } 
+      else if (legitPuzzle === "Expected puzzle to be 81 characters long"){
+        res.json({error:"Expected puzzle to be 81 characters long"}); 
+      } 
+      else {
+        let solution = solver.solve(legitPuzzle); 
+        if (solution === "Puzzle cannot be solved"){
+          res.json({error:"Puzzle cannot be solved"})
+        } 
+        else{
+          res.send({solution});
+        } 
+      }
     });
 };
