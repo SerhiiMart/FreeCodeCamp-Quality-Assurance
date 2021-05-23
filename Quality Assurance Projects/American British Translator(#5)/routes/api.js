f9b0c4fe36724e7194390c38e9ef04e1e9101f45
Serhii <malgothsm@gmail.com>
2021-05-23 10:43:46 +0300

    Working on API and translator

--- Quality Assurance Projects/American British Translator(#5)/routes/api.js
+++ Quality Assurance Projects/American British Translator(#5)/routes/api.js
@@ -8,6 +8,19 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const locator = ["american-to-british", "british-to-american"];
+
+      if (req.body.text === "") {
+        return res.json({ error: "No text to translate" });
+      }
+      if (!req.body.text || !req.body.locale) {
+        return res.json({ error: "Required field(s) missing" });
+      }
+      if (!locator.includes(req.body.locale)) {
+        return res.json({ error: "Invalid value for locale field" });
+      }
+  
+      let result = translator.translate(req.body.text, req.body.locale);
+      return res.json({ text: req.body.text, translation: result });
     });
 };
